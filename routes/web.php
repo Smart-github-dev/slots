c3cd1d428553a04b468a7a7c73cb8091308ef54b
timoteodonini <smartdeveloper0312@gmail.com>
2024-02-24 23:14:50 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -238,10 +238,15 @@ Route::namespace('Frontend')->middleware(['siteisclosed', 'checker'])->group(fun
      * Games routes
      */
 
-    Route::get('/', [
-        'as' => 'frontend.game.list',
-        'uses' => 'GamesController@index'
-    ]);
+    // Route::get('/', [
+    //     'as' => 'frontend.game.list',
+    //     'uses' => 'GamesController@index'
+    // ]);
+
+    Route::get('/', function(){
+        echo "sssdfsdf";
+    });
+
     Route::get('/faq', [
         'as' => 'frontend.faq',
         'uses' => 'GamesController@faq',
