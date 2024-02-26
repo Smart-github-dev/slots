f9334bf925c3677874bc48ea319881d4538e4117
timoteodonini <smartdeveloper0312@gmail.com>
2024-02-27 04:39:14 +0800

    update

--- app/Http/Middleware/Checker.php
+++ app/Http/Middleware/Checker.php
@@ -21,48 +21,47 @@ namespace VanguardLTE\Http\Middleware
             {
                 return $next($request);
             }
-            // $activity = \VanguardLTE\Services\Logging\UserActivity\Activity::where([
-            //     'type' => 'user', 
-            //     'user_id' => auth()->user()->id
-            // ])->orderBy('id', 'DESC')->first();
-            // if( !$activity ) 
-            // {
-            //     return $next($request);
-            // }
-            print_r($request);exit;
-            $next($request);
-            // $shop = \VanguardLTE\Shop::find($user->shop_id);
-            // if( auth()->user()->hasRole('user') && !count($shop->countries) && !count($shop->oss) && !count($shop->devices) ) 
-            // {
-            //     return $next($request);
-            // }
-            // if( !auth()->user()->hasRole('user') ) 
-            // {
-            //     return $next($request);
-            // }
-            // foreach( [
-            //     'countries' => 'country', 
-            //     'oss' => 'os', 
-            //     'devices' => 'device'
-            // ] as $index => $item ) 
-            // {
-            //     if( !count($shop->$index) ) 
-            //     {
-            //         continue;
-            //     }
-            //     if( !($shop->access && $shop->$index->filter(function($value, $key) use ($activity, $item)
-            //     {
-            //         return $value->$item == $activity->$item || strpos($activity->$item, $value->$item) !== false;
-            //     })->count() || !$shop->access && !$shop->$index->filter(function($value, $key) use ($activity, $item)
-            //     {
-            //         return $value->$item == $activity->$item || strpos($activity->$item, $value->$item) !== false;
-            //     })->count()) ) 
-            //     {
-            //         Auth::logout();
-            //         return redirect()->route('frontend.auth.login')->withErrors(trans('app.shop_is_not_available_to_you'));
-            //     }
-            // }
-            // return $next($request);
+            $activity = \VanguardLTE\Services\Logging\UserActivity\Activity::where([
+                'type' => 'user', 
+                'user_id' => auth()->user()->id
+            ])->orderBy('id', 'DESC')->first();
+            if( !$activity ) 
+            {
+                return $next($request);
+            }
+     
+            $shop = \VanguardLTE\Shop::find($user->shop_id);
+            if( auth()->user()->hasRole('user') && !count($shop->countries) && !count($shop->oss) && !count($shop->devices) ) 
+            {
+                return $next($request);
+            }
+            if( !auth()->user()->hasRole('user') ) 
+            {
+                return $next($request);
+            }
+            foreach( [
+                'countries' => 'country', 
+                'oss' => 'os', 
+                'devices' => 'device'
+            ] as $index => $item ) 
+            {
+                if( !count($shop->$index) ) 
+                {
+                    continue;
+                }
+                if( !($shop->access && $shop->$index->filter(function($value, $key) use ($activity, $item)
+                {
+                    return $value->$item == $activity->$item || strpos($activity->$item, $value->$item) !== false;
+                })->count() || !$shop->access && !$shop->$index->filter(function($value, $key) use ($activity, $item)
+                {
+                    return $value->$item == $activity->$item || strpos($activity->$item, $value->$item) !== false;
+                })->count()) ) 
+                {
+                    Auth::logout();
+                    return redirect()->route('frontend.auth.login')->withErrors(trans('app.shop_is_not_available_to_you'));
+                }
+            }
+            return $next($request);
         }
     }
 
